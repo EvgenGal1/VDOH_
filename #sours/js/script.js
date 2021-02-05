572c036ea3bf7915844f247dc217167aced4f420
EvgenGal1 <EvgenGal1@users.noreply.github.com>
2021-02-06 02:04:48 +0300

    все файлы изменены. header li md, kit, scss, js, txt

--- #sours/js/script.js
+++ #sours/js/script.js
@@ -1,8 +1,10 @@
-console.log(12);
+// <script.js>˅=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=˅
+
+// ibg - совместно с .ibg путь картинки делает фоном у родителя
+console.log(1);
 function ibg() {
-  console.log(0);
+  console.log(2);
   $.each($(".ibg"), function (index, val) {
-    console.log(1);
     if ($(this).find("img").length > 0) {
       $(this).css(
         "background-image",
@@ -12,3 +14,5 @@ function ibg() {
   });
 }
 ibg();
+
+//</script.js>˄=¡=¡=¡=¡=¡=!=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=˄
