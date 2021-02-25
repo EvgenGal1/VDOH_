2493b4f2d9e6adca5fc52ea7fe63ce5a347afde0
EvgenGal1 <EvgenGal1@users.noreply.github.com>
2021-02-26 02:43:03 +0300

    подправил шапку ul-li, нач. общие классы катал и катал-итем у img > катал-game

--- #sours/js/main.js
+++ #sours/js/main.js
@@ -1,7 +1,8 @@
 //<main.js(сбор всех скриптов)>˅=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=˅
 
 //@prepros-append map.js
-//@prepros-append game/games.js
+// ??? не раб - .ibg е/и script.js ниже game.js.
 //@prepros-append script.js
+//@prepros-append game/games.js
 
 //</main.js(сбор всех скриптов)>˄=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=˄
