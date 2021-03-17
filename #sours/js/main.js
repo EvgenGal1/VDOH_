08d5667592e8c5439be6ebfe0305fb9f13aede32
EvgenGal1 <EvgenGal1@users.noreply.github.com>
2021-03-17 04:26:23 +0300

    ++ .ibg на JS, общ. файл scriptJquery.kit, начало parseTheme.scss(измен. стилей по нажатию)

--- #sours/js/main.js
+++ #sours/js/main.js
@@ -1,6 +1,5 @@
 //<main.js(сбор всех скриптов)>˅=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=˅
 
-//@prepros-append map.js
 // ??? не раб - .ibg е/и script.js ниже game.js.
 //@prepros-append script.js
 //@prepros-append game/games.js
--- #sours/js/script.js
+++ #sours/js/script.js
@@ -1,6 +1,7 @@
 // <script.js>˅=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=˅
 
-// ibg - совместно с .ibg путь картинки делает фоном у родителя
+// ibg - совместно с .ibg - путь картинки делает фоном у родителя
+// на JQuery
 function ibg() {
   $.each($(".ibg"), function (index, val) {
     if ($(this).find("img").length > 0) {
@@ -12,5 +13,33 @@ function ibg() {
   });
 }
 ibg();
+
+// на JS
+// function ibg() {
+//   let ibg = document.querySelectorAll(".ibg");
+//   for (var i = 0; i < ibg.length; i++) {
+//     if (
+//       ibg[i].querySelector("img") &&
+//       ibg[i].querySelector("img").getAttribute("src") != null
+//     ) {
+//       ibg[i].style.backgroundImage =
+//         "url(" + ibg[i].querySelector("img").getAttribute("src") + ")";
+//     }
+//   }
+// }
+// ibg();
+
+// не доделан на JS
+// попытка на JS
+// function ibg() {
+//   console.log(1);
+//   var privClass = document.getElementsByClassName("block-img__link");
+//   privClass[0].style.backgroundImage = "url('../../img/head.jpg')";
+//   // if (getPrivClass) {
+//   //   var getImgTag = document.body.getElementsByTagName("img");
+//   //   var getIbg = document.body.getElementsByClassName("ibg");
+//   // }
+// }
+// ibg();
 
 //</script.js>˄=¡=¡=¡=¡=¡=!=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=¡=˄
